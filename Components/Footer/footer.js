0b0a9e29d765dd63d2f3d986d0c70f69119b36e8
Rizona <rizonehyseni@gmail.com>
2025-11-16 20:54:18 +0100

    made the footer responsive

--- Components/Footer/footer.js
+++ Components/Footer/footer.js
@@ -1,60 +1,55 @@
 class FooterComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-     
-  <footer>
-    <div class="footer_container">
-      <div class="footer_col">
+  <link rel="stylesheet" href="/Components/Footer/footer.css">
+   <footer class="footer">
+    <div class="links-container">
+      <div class="logo-section">
+        <a href="/HTML/index.html"><img src="/photos/IlSogno.png" width="210px" alt="Restaurant Logo"></a>
+        <p>Enjoy our delicious cuisine and warm ambiance.</p>
 
-        <div class="footer_logo">
-          <a href="index.html"><img src="/photos/IlSogno.png" alt=""></a>
-        </div>
-        <p class="footer_text">
-          Enjoy our delicious cuisine and warm ambiance.
-        </p>
-        <ul class="footer_socials">
-          <li><a href="#"><img src="/photos/icons8-facebook-logo-50.png" alt=""></a></li>
-          <li><a href="#"><img src="/photos/icons8-instagram-50.png" alt=""></a></li>
+      </div>
+      <div>
+        <h3>Details</h3>
+        <ul>
+          <li><a href="/HTML/menu.html">menu</a></li>
+          <li><a href="/HTML/menu.html" target="blank">reservations</a></li>
+          <li><a href="/HTML/music.html">music</a></li>
+          <li><a href="/HTML/location.html">location</a></li>
+          <li><a href="/HTML/gallery.html">gallery</a></li>
 
         </ul>
       </div>
 
-      <div class="footer_col">
-        <h4>Details</h4>
-        <ul class="footer_links">
-          <li><a href="menu.html">menu</a></li>
-          <li><a href="reservations.html" target="blank">reservations</a></li>
-          <li><a href="music.html">music</a></li>
-          <li><a href="location.html">location</a></li>
-        </ul>
-      </div>
-
-      <div class="footer_col">
-        <h4>Other</h4>
-        <ul class="footer_links">
+      <div>
+        <h3>Other</h3>
+        <ul>
           <li><a href="/PRIVACY_POLICY.md">Privacy Policy</a></li>
           <li><a href="/TERMS_AND_CONDITIONS.md">Terms and Conditions</a></li>
+
+
         </ul>
       </div>
 
-      <div class="footer_col">
-        <h4>Contact Us</h4>
-        <div class="footer_links">
-          <a class="footer_text" href="tel:+38344000000">
-            +383 44 000 000
-          </a>
-          <a class="email_contact" href="mailto:rizonehyseni@gmail.com">
-            ilsogno@restaurant.com</a>
-        </div>
+      <div>
+        <h3>Address</h3>
+        <ul>
+          <li><a href="address:123 Main Street,Amalfi Coast, Italy">123 Main Street,Amalfi Coast, Italy</a></li>
+        </ul>
+
+        <h3>Contact Us</h3>
+        <ul>
+          <li><a href="mailto:ilsogno@restaurant.com">ilsogno@restaurant.com</a></li>
+        </ul>
+
       </div>
-
-
-
     </div>
+
     <div class="footer_bar">
-      copyright @ 2025 Il Sogno Restaurant. All rights reserved.
+      <p>copyright &copy; 2025 Il Sogno Restaurant. All rights reserved.</p>
     </div>
-  </footer>
+
+    </footer>
     `;
   }
 }
