a0b6313acdbd74875390752fb54d23d523bf8020
Siona <sisimustafa31@gmail.com>
2025-11-28 00:38:30 +0100

    .

--- JS/gallery.js
+++ JS/gallery.js
@@ -1,31 +1,5 @@
 
 
-//typewriter effect for gallery introuction
-
-
-const galleryTitle = document.getElementById('gallery_title');
-const galleryDesc = document.getElementById('gallery_desc');
-
-const title = 'Galleria dei Sogni';
-const desc = ' Step into a visual journey through Il Sogno - where art, flavor, and atmosphere come together. Each image captures a moment of inspiration, from the warm lights of our bar to the elegance of Italian craftsmanship.';
-
-let i = 0;
-let j = 0;
-let hasTyped = false;
-
-const typeWriter = () => {
-  if (i < title.length) {
-    galleryTitle.textContent += title.charAt(i);
-    i++;
-    setTimeout(typeWriter, 40);
-  } else if (j < desc.length) {
-    galleryDesc.textContent += desc.charAt(j);
-    j++;
-    setTimeout(typeWriter, 20);
-  }
-};
-
-
 
 
 //images load so they appear ma smooth
@@ -48,10 +22,7 @@ const images = document.querySelectorAll('.images_container');
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    if (entry.isIntersecting && !hasTyped) {
-      typeWriter();
-      hasTyped = true;
-    }
+    
 
 
 
@@ -64,8 +35,6 @@ const observer = new IntersectionObserver(entries => {
   });
 });
 
-observer.observe(galleryTitle);
-observer.observe(galleryDesc);
 
 //to observe all images
 
