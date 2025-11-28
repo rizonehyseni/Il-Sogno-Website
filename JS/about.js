8a016dc5dfac959f924d06000ddb281875d4e2a4
Rizona <rizonehyseni@gmail.com>
2025-11-28 01:46:46 +0100

    added animation for about page

--- JS/about.js
+++ JS/about.js
@@ -11,3 +11,38 @@ document.addEventListener('DOMContentLoaded', () => {
     fadeObserver.observe(el);
   });
 });
+
+
+
+
+const heroText = "Enjoy our delicious cuisine and warm ambiance.";
+const heroElement = document.getElementById("hero-typewriter");
+
+let i = 0;
+let hasStarted = false;
+
+function typeHero() {
+  if (hasStarted) return;
+  hasStarted = true;
+
+  function type() {
+    if (i < heroText.length) {
+      heroElement.textContent += heroText.charAt(i);
+      i++;
+      setTimeout(type, 50);
+    }
+  }
+  type();
+}
+
+
+const heroObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      typeHero();
+      heroObserver.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.5 });
+
+heroObserver.observe(heroElement);
