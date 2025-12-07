80f640f7f9e6a6bac04e47e73263816cc6192ede
Rizona <rizonehyseni@gmail.com>
2025-12-07 13:17:56 +0100

    stilova select time calendarn

--- JS/reservations.js
+++ JS/reservations.js
@@ -1,7 +1,15 @@
+//koha
 flatpickr("#time-picker", {
   enableTime: true,
   noCalendar: true,
   dateFormat: "H:i",
   time_24hr: true,
-  minuteIncrement: 30,
+  minuteIncrement: 15,
+});
+
+//data
+flatpickr("#date-picker", {
+  dateFormat: "Y-m-d",
+  minDate: "2025-12-23",
+  maxDate: "2026-01-31",
 });
