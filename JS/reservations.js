18dfccd00df1784e7c99dd6a76c8704d6c1f8ba8
Rizona <rizonehyseni@gmail.com>
2025-12-07 14:10:47 +0100

    validimi i emrit

--- JS/reservations.js
+++ JS/reservations.js
@@ -13,3 +13,23 @@ flatpickr("#date-picker", {
   minDate: "2025-12-23",
   maxDate: "2026-01-31",
 });
+
+
+//validimi i numri te telefonit
+
+
+
+
+
+
+//Validimi per full name
+document.addEventListener("DOMContentLoaded", () => {
+  const name = document.querySelector('input[placeholder="Full Name"]');
+
+  name.addEventListener("input", () => {
+    name.value = name.value
+      .replace(/[0-9]/g, "")           // hiq numrat
+      .slice(0, 30)                    // maksimum 20 karaktere gjithsej
+      .replace(/\b\w/g, c => c.toUpperCase()); // cdo fjale me shkronje te madhe
+  });
+});
