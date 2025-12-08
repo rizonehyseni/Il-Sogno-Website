0736513ef7611bdad1abf6eb3577b6073e1a18bc
Rrexon <rrezon.ibishi@gmail.com>
2025-12-08 17:57:58 +0100

    new js animation for headers

--- JS/location.js
+++ JS/location.js
@@ -5,4 +5,59 @@ document.addEventListener("DOMContentLoaded", function () {
   lines.forEach(function(line) {
     line.style.width = "100%";
   });
+
+  const text = `HOW TO FIND US`;
+  const typingText = document.getElementById("find-us-header");
+
+  let i = 0;
+  let hasTyped = false;
+
+  function typeWriter() {
+    if (i < text.length) {
+      typingText.textContent += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, 40);
+    }
+  }
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const element = entry.target;
+
+      if (element.id === "find-us-header" && entry.isIntersecting && !hasTyped) {
+        hasTyped = true;
+        typeWriter();
+      }
+    });
   });
+
+  observer.observe(typingText);
+
+    const text1 = `Would you like to contact us for further information?`;
+  const typingText1 = document.getElementById("contactus");
+
+  let i1 = 0;
+  let hasTyped1 = false;
+
+  function typeWriter1() {
+    if (i1 < text1.length) {
+      typingText1.textContent += text1.charAt(i1);
+      i1++;
+      setTimeout(typeWriter1, 40);
+    }
+  }
+
+  const observer1 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const element = entry.target;
+
+      if (element.id === "contactus" && entry.isIntersecting && !hasTyped1) {
+        hasTyped1 = true;
+        typeWriter1();
+      }
+    });
+  });
+
+  observer1.observe(typingText1);
+});
+
