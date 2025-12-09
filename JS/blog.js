5622bda87dda73f2a1aa284ea1cb97028e78cbb3
Rrexon <rrezon.ibishi@gmail.com>
2025-12-09 23:27:48 +0100

    updated class and id names for easier  formatting

--- JS/blog.js
+++ JS/blog.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const text = `Welcome to Il Sogno, where every dish is crafted with passion, bringing the true flavors of Italy straight to your table.
 Join us and savor moments made memorable.`;
   
-  const typingText = document.getElementById("divider-name");
+  const typingText = document.getElementById("hero-title");
 
   let i = 0;
 
