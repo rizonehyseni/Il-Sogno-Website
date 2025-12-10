e32bb7d96b27fe3cad489f1c6d6155a15469b8a2
Siona <sisimustafa31@gmail.com>
2025-12-10 21:18:54 +0100

    navbari per react

--- react-page/src/App.jsx
+++ react-page/src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Navbar = ()=>{
+  return(
+     <div className="navbar">
+        <div className="navbar_container">
+          <a href="index.html" class="logo_link">
+            <img src="/photos/IlSogno.png" alt="Logo" className="logo"></img>
+          </a>
 
-function App() {
-  const [count, setCount] = useState(0)
+          <ul className="nav_menu">
+            <li className="nav_item"><a href="index.html" className="nav_link">Home</a></li>
+            <li className="nav_item"><a href="menu.html" className="nav_link">Menu</a></li>
+            <li className="nav_item"><a href="music.html" className="nav_link">Music</a></li>
+            <li className="nav_item"><a href="reservations.html" target="_blank" className="nav_link">Reservations</a></li>
+            <li className="nav_item more_nav">
+              <button type="button" className="nav_link" id="more">More ▾</button>
+
+             
+            </li>
+             <ul className="nav_menu_secondary" id="nav_menu_secondary">
+                <li className="nav_item"><a href="location.html" className="nav_link">Location</a></li>
+                <li className="nav_item"><a href="tickets.html" className="nav_link">Tickets</a></li>
+                <li className="nav_item"><a href="gallery.html" className="nav_link">Gallery</a></li>
+                <li className="nav_item"><a href="blog.html" className="nav_link">Blog</a></li>
+                <li className="nav_item"><a href="about.html" className="nav_link">About Us</a></li>
+              </ul>
+          </ul>
+
+          <div className="navbar_toggle" id="mobile_menu">
+            <span className="bar">-</span>
+            <span className="bar">-</span>
+            <span className="bar">-</span>
+          </div>
+        </div>
+      </div>
+  )
+}
+const App = () => {
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+    <Navbar/>
+    </div>
   )
 }
 
