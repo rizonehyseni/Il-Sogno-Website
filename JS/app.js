decea9cc35f584719108059ef20ee2c36f0efa14
Siona <sisimustafa31@gmail.com>
2025-12-31 00:03:17 +0100

    ...

--- JS/app.js
+++ JS/app.js
@@ -8,28 +8,33 @@ window.addEventListener("load", () => {
     document.body.classList.remove("loading");
   }, 3500);
 });
+//body tagut po ja len klasen loading derisa te keryhet animacioni per intro
+//mas 3.5s intro containerin e mshefum
 
 
-
-
+//teksti qe duhet mu shfaq ne faqe
 const text = `"Music is the cup which holds the wine of silence."`;
 const text2 = "- Robert Fripp";
 const text3 =
   "Chef Luca reveals the secrets of truffle season — from selecting the finest local varieties to crafting dishes that capture their earthy aroma.";
 
+  //var per qato eleemnte
 const typingText = document.getElementById("divider_quote");
 const typingText2 = document.getElementById("divider_author");
 const typingText3 = document.getElementById("blog_info");
 
+
+//iterator per secilen fjali
 let i = 0,
   j = 0,
   k = 0;
-let hasTyped = false;
+let hasTyped = false;//qe me kontrollu kur bohet animacioni
+
 
 function typeWriter() {
   if (i < text.length) {
-    typingText.textContent += text.charAt(i++);
-    setTimeout(typeWriter, 40);
+    typingText.textContent += text.charAt(i++);//po ja shtojna elementit textin e fjalise
+    setTimeout(typeWriter, 40);//mas cdo 40ms e thirr typewriterin apet
   } else if (j < text2.length) {
     typingText2.textContent += text2.charAt(j++);
     setTimeout(typeWriter, 40);
@@ -40,21 +45,22 @@ function typeWriter() {
 }
 
 
-
+//observeri
 const aboutText = document.querySelector(".about_image_text");
 
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      if (!entry.isIntersecting) return;
+      if (!entry.isIntersecting) return;//nese elementi nuk doket duhet mu nderpre
 
       // About image text
-      if (entry.target.classList.contains("about_image_text")) {
+      if (entry.target.classList.contains("about_image_text")) {//pe targeton elementin
         entry.target.classList.add("show");
         observer.unobserve(entry.target);
       }
 
       // Typewriter trigger
+      //nese elementii targetum e ka id, dhe nuk ka shkru, atehere ...
       if (
         (entry.target.id === "divider_quote" ||
           entry.target.id === "blog_info") &&
@@ -79,27 +85,30 @@ observer.observe(typingText3);
 
 
 
-//me scrollreveal
+//me scrollreveal(librari)
+
 document.addEventListener("DOMContentLoaded", () => {
+  //1 animacion
   const sr = ScrollReveal({
     distance: "40px",
     duration: 700,
     easing: "ease-out",
     opacity: 0,
-    reset: false,
-    mobile: false
+    reset: false,//animacioni ndodh veq 1 here
+    mobile: false//jo per mobile
   });
 
+  //elementet qe i targeton
   sr.reveal(".about_card", {
-     origin: "bottom",
-  interval: 150,
+     origin: "bottom",//prej ku ja nis animacioni
+     interval: 150,//sa me prit per mu shfaq tjetri element
   beforeReveal: el => {
-    el.style.transform = "none";
+    el.style.transform = "none";//e hjek qat transformin qe j bon vete libraria (distance) para se mu shfaq el
   }
   });
 
   sr.reveal(".menu_img", { origin: "bottom" });
-  sr.reveal(".menu_title", { origin: "bottom", delay: 100 });
+  sr.reveal(".menu_title", { origin: "bottom", delay: 100 });//sa mu vonu per mw kalu te tjetra
   sr.reveal(".menu_desc", { origin: "bottom", delay: 150 });
   sr.reveal(".menu_button", { origin: "bottom", delay: 200 });
 
