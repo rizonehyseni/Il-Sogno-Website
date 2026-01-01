68f52e274053cdc3a2a1f2b305f383ad2f309efb
Rizona <rizonehyseni@gmail.com>
2026-01-01 16:19:25 +0100

    js kodi per timeline

---
+++ JS/about.js
@@ -0,0 +1,35 @@
+
+// js kodi per timeline
+const timeline = document.querySelector(".timeline");
+const line = document.querySelector(".timeline-innerline");
+const items = document.querySelectorAll(".timeline ul li");
+
+let started = false;
+
+function startTimeline() {
+  items.forEach((item, index) => {
+    setTimeout(() => {
+      item.classList.add("active");
+
+      const progress = ((index + 1) / items.length) * 100;
+
+      if (window.innerWidth >= 728) {
+        line.style.width = progress + "%";
+      } else {
+        line.style.height = progress + "%";
+      }
+    }, index * 1000);
+  });
+}
+
+const observer = new IntersectionObserver(
+  ([entry]) => {
+    if (entry.isIntersecting && !started) {
+      started = true;
+      startTimeline();
+    }
+  },
+  {
+    threshold: 0.5, // bohet trigger kur osht visible 0.5%
+  }
+);
